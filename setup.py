38a6f6f59b8f549af04b82b2d514721159e8b4ed
Greg <greg@yhatHQ.com>
2014-02-18 12:34:21 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='busby',
-    version='0.7',
+    version='0.8',
     author="Jessie Frazelle",
     author_email="jessie@yhathq.com",
     url="https://github.com/yhat/busby",
